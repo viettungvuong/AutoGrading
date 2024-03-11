39512b307789ee45840ea26221f5d17023600584
viettungvuong <viettungvuong@gmail.com>
2024-03-11 16:11:45 +0700

    Flutter kết nối vs backend

---
+++ auto_grading_app/lib/constant.dart
@@ -0,0 +1,1 @@
+String serverUrl="http://127.0.0.1:8000";
---
+++ auto_grading_app/lib/controllers/backendService.dart
@@ -0,0 +1,19 @@
+import 'package:auto_grading_mobile/constant.dart';
+import 'package:camera/camera.dart';
+import 'package:http/http.dart' as http;
+import 'package:http_parser/http_parser.dart';
+
+void ConnectToGrade(XFile image, int availableChoices){
+  var url = Uri.parse(serverUrl);
+
+  var request = new http.MultipartRequest("POST", url); // gửi qua server để chấm bài
+  request.fields['available_choices'] = availableChoices.toString();
+  request.files.add(http.MultipartFile.fromPath(
+    'file',
+    image.path,
+    contentType: new MediaType('image'),
+  ) as http.MultipartFile);
+  request.send().then((response) {
+    if (response.statusCode == 200) print("Uploaded!");
+  });
+}
